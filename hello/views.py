2d7d566baa7869b4afc02bf8c123639dbe03b872
Dave Jagoda <davejagoda@gmail.com>
2015-09-01 16:13:11 -0700

    put a space at the end of the string so it is easier to read

--- hello/views.py
+++ hello/views.py
@@ -9,7 +9,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     times = int(os.environ.get('TIMES',3))
-    return HttpResponse('こんにちは' * times)
+    return HttpResponse('こんにちは ' * times)
 ##    r = requests.get('http://httpbin.org/status/418')
 ##    print r.text
 ##    return HttpResponse('<pre>' + r.text + '</pre>')
